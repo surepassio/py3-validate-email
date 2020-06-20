c9904777bb1f8d32b9b59576435559874d3626b2
thec0sm0s <deepakrajko14@gmail.com>
2020-06-20 12:27:39 +0530

    Bump version to 0.3.0

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ class BuildPyCommand(build_py):
 
 setup(
     name='py3-validate-email',
-    version='0.2.9',
+    version='0.3.0',
     packages=find_packages(exclude=['tests']),
     install_requires=['dnspython~=1.16', 'idna~=2.8', 'filelock~=3.0', 'PySocks'],
     author='László Károlyi',
